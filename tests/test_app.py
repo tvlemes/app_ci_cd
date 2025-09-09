ca73260d240680f95166b8ac74415566c36c784d
Thiago Lemes <contatothiagolemes@gmail.com>
2025-09-09 14:46:42 -0300

    test deploy

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
-# import pytest
-# import app
+import pytest
+import app
+
+def test_app_runs():
+    print("Teste realizado com sucesso!") 
+
 
 # def test_app_runs():
 #     assert hasattr(app, "st")  # garante que streamlit foi importado
